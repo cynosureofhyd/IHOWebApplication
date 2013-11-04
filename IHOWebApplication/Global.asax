3ae3312866a2fd2a9235f3ad24f6425dfe20b4d8
cynosureofhyd2 <tomu646@gmail.com>
2013-11-03 17:48:26 -0700

    First Commit of IHO Web Application

---
+++ IHOWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IHOWebApplication.MvcApplication" Language="C#" %>
---
+++ IHOWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IHOWebApplication.Startup))]
+namespace IHOWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
