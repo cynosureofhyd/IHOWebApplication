9a39da5602c0bb09d553ccc4cbf1b80123ac4669
cynosureofhyd2 <tomu646@gmail.com>
2013-11-03 22:05:28 -0700

    Get Kendo Rich Text Editor Working

--- IHOWebApplication/App_Start/BundleConfig.cs
+++ IHOWebApplication/App_Start/BundleConfig.cs
@@ -11,6 +11,8 @@ namespace IHOWebApplication
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            //bundles.Add(new ScriptBundle("~/Scripts/kendo/2013.2.716/").Include("~/Scripts/kendo/2013.2.716/*.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
