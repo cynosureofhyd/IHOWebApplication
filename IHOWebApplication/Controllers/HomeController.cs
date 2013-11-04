8859487769a01dc44a89822661e0e8912a4c8261
cynosureofhyd2 <tomu646@gmail.com>
2013-11-03 18:12:01 -0700

    first few changes to IHO Web Application

--- IHOWebApplication/Controllers/HomeController.cs
+++ IHOWebApplication/Controllers/HomeController.cs
@@ -20,11 +20,17 @@ namespace IHOWebApplication.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Connect()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult NewsandEvents()
+        {
+
+            return View("Partial1");
         }
     }
 }
---
+++ IHOWebApplication/Models/NewsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+namespace IHOWebApplication.Models
+{
+    public class NewsModel
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public List<Image> Images { get; set; }
+    }
+}
